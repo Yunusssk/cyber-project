8095fca32d07c21fe029535d93eddd0fcb2b5832
Yunusssk <152277962+Yunusssk@users.noreply.github.com>
2024-05-07 12:05:58 +0300

    Projeye react router dom eklendi

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,21 @@
-import {ReduxCount} from './components/redux/Count'
-import './App.css';
+import { Route, Routes } from 'react-router-dom';
+ import './App.css';
+ import Home from './components/HomePage/Navbar';
+ import About from './components/AboutPage/About';
+ import Contact from './components/ContactPage/Contact';
+ import Blog from './components/BlogPage/Blog';
 
-function App() {
-  return (
-    <div className="App">
-      <ReduxCount />
-    
-    </div>
-  );
-}
+ function App() {
+   return (
+     <div className="App">
+      <Routes>
+         <Route path='/' element={<Home/>} />
+         <Route path='/about' element={<About/>} />
+         <Route path='/contact' element={<Contact/>} />
+         <Route path='/blog' element={<Blog/>} />
+       </Routes>
+     </div>
+   );
+ }
 
-export default App;
+ export default App;
--- src/main.jsx
+++ src/main.jsx
@@ -1,16 +1,20 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
+// import './index.css';
 import App from './App';
 import { Provider } from 'react-redux'
 import { Store } from './components/redux/Store';
+import { BrowserRouter } from 'react-router-dom';
+
 
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <Provider store={Store}>
-       <App />
-    </Provider>
+    <BrowserRouter>
+      <Provider store={Store}>
+        <App />
+      </Provider>
+    </BrowserRouter >
   </React.StrictMode>
 );
