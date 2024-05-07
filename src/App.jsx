d7b6e0c34517641ddbc875c06a2ee58cdf987f7e
Yunusssk <152277962+Yunusssk@users.noreply.github.com>
2024-05-07 08:29:16 +0300

    Projeye json server ve react-redux eklendi

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import {ReduxCount} from './components/redux/Count'
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <ReduxCount />
+    
+    </div>
+  );
+}
+
+export default App;
