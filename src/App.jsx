ca53b2cebca0e99f20ca2731d408062be5140961
Yunusssk <152277962+Yunusssk@users.noreply.github.com>
2024-05-24 11:52:59 +0300

    Projeye react router dom eklendi

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,15 @@
-import { Route, Routes } from 'react-router-dom';
+import {Route, Routes, useParams} from 'react-router-dom';
  import './App.css';
  import Home from './components/HomePage/Navbar';
  import About from './components/AboutPage/About';
  import Contact from './components/ContactPage/Contact';
  import Blog from './components/BlogPage/Blog';
+import Body from "./components/HomePage/Body.jsx";
+import Footer from "./components/HomePage/Footer.jsx";
+import ProductDetail from "./components/ProductDetailPage/ProductDetail.jsx";
+
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
 
  function App() {
    return (
@@ -11,9 +17,16 @@ import { Route, Routes } from 'react-router-dom';
       <Routes>
          <Route path='/' element={<Home/>} />
          <Route path='/about' element={<About/>} />
-         <Route path='/contact' element={<Contact/>} />
+         <Route path='/contactus' element={<Contact/>} />
          <Route path='/blog' element={<Blog/>} />
+         <Route path='/favorite' element={<Blog/>} />
+         <Route path='/cart' element={<Blog/>} />
+         <Route path='/person' element={<Blog/>} />
+          {/* eslint-disable-next-line no-undef */}
+          <Route path="/productDetail/:id" element={<ProductDetail />} />
        </Routes>
+         <Body />
+         <Footer />
      </div>
    );
  }
